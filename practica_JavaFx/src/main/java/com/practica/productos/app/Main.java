c9ccccbb89589fac19accf02d54a70f4c8b97068
umg <umg@HUE-LAB-2I2.labhue.local>
2026-05-09 10:51:18 -0600

    Integrate service with UI

--- practica_JavaFx/src/main/java/com/practica/productos/app/Main.java
+++ practica_JavaFx/src/main/java/com/practica/productos/app/Main.java
@@ -5,21 +5,29 @@
 package com.practica.productos.app;
 
 import com.practica.productos.modelo.Producto;
+import com.practica.productos.servicio.ProductoService;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.stage.Stage;  
+
 
 public class Main extends Application {
+    ProductoService servicio = new ProductoService();
     @Override
     public void start(Stage stage) {
     TextField campo = new TextField();
     Button boton = new Button("Mostrar");
     Label label = new Label();
     boton.setOnAction(e -> {
+        servicio.agregar(new Producto(campo.getText()));
+String texto = "";
+for (Producto p : servicio.listar()) {
+texto += p.getNombre() + "\n";
+}
       try {
     Producto p = new Producto(campo.getText());
     label.setText(p.getNombre());
---
+++ practica_JavaFx/src/main/java/com/practica/productos/servicio/ProductoService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.practica.productos.servicio;
+
+import com.practica.productos.modelo.Producto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoService {
+  
+    private List<Producto> productos = new ArrayList<>();
+
+    public void agregar(Producto p) {
+        productos.add(p);
+    }
+
+    public List<Producto> listar() {
+     
+        return new ArrayList<>(productos);
+    }
+}
