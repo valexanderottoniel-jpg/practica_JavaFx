249864561fc1131530d604e89aa37d0321f6dd42
umg <umg@HUE-LAB-2I2.labhue.local>
2026-05-09 09:50:31 -0600

    Initial project structure

---
+++ practica_JavaFx/src/main/java/com/mycompany/practica_javafx/Practica_JavaFx.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_javafx;
+
+/**
+ *
+ * @author umg
+ */
+public class Practica_JavaFx {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
