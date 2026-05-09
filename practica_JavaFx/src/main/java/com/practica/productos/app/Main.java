5dec44cdaf3702cca07901300fd89e1cb1d55f58
umg <umg@HUE-LAB-2I2.labhue.local>
2026-05-09 10:31:48 -0600

    Add basic UI interaction

--- practica_JavaFx/src/main/java/com/practica/productos/app/Main.java
+++ practica_JavaFx/src/main/java/com/practica/productos/app/Main.java
@@ -6,16 +6,25 @@ package com.practica.productos.app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Hola JavaFX");
-        Scene scene = new Scene(new VBox(label), 300, 200);
-        stage.setScene(scene);
-        stage.show();
-    }
+    TextField campo = new TextField();
+    Button boton = new Button("Mostrar");
+    Label label = new Label();
+    boton.setOnAction(e -> {
+        label.setText(campo.getText());
+    });
+    VBox layout = new VBox(10, campo, boton, label);
+    
+    Scene scene = new Scene(layout, 300, 200);
+    stage.setScene(scene);
+    stage.show();
+}
 }
