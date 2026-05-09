e686e3b05315e2f578fb2a175025373a419daf4f
umg <umg@HUE-LAB-2I2.labhue.local>
2026-05-09 10:09:07 -0600

    Add JavaFX base window

---
+++ practica_JavaFx/src/main/java/com/practica/productos/app/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.practica.productos.app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hola JavaFX");
+        Scene scene = new Scene(new VBox(label), 300, 200);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
